3d9b39eaa4b0147a1165aab83375a75caf983e4f
LiQian-XC <liqian.picb@gmail.com>
2022-02-15 22:03:10 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='scTour',
+    version='0.0.1',
+    author='Qian Li',
+    author_email='liqian.picb@gmail.com',
+    description='a tool for robust time, transcriptome, trajectory inference',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'torch',
+        'torchdiffeq',
+        'numpy',
+        'scanpy',
+        'anndata',
+        'scipy'
+        'tqdm',
+    ]
+)
